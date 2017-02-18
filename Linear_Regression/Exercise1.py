b739f207a534ad0e9d0c49c187eb87c0f83eb526
Mark Evans <meutband@gmail.com>
2017-02-17 22:37:07 -0600

    Add files via upload

---
+++ Linear_Regression/Exercise1.py
@@ -0,0 +1,228 @@
+import statsmodels.api as sm
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+import plotly.plotly as py
+import plotly.graph_objs as go
+from statsmodels.graphics import regressionplots as smg
+
+
+prestige = sm.datasets.get_rdataset("Duncan", "car", cache=True).data
+credit_card = sm.datasets.ccard.load_pandas().data
+
+## Part 1: Linear Regression Diagnostics
+
+'''
+
+1. Explore the dataset with a scatter_matrix and a boxplot. Fit a linear
+regression model to each of the datasets. Print and examine the summaries of
+the models.
+
+'''
+
+#Scatter Matrix
+pd.scatter_matrix(prestige, diagonal='kde')
+plt.savefig('images1/prest_matrix.png')
+pd.scatter_matrix(credit_card, diagonal='kde')
+plt.savefig('images1/credit_matrix.png')
+
+#Boxplot
+prestige.boxplot('income')
+plt.savefig('images1/prest1_boxplot.png')
+prestige.boxplot('income', by = 'type')
+plt.savefig('images1/prest2_boxplot.png')
+
+credit_card.boxplot('AGE', by = 'OWNRENT')
+plt.savefig('images1/credit1_boxplot.png')
+credit_card.boxplot('AVGEXP', by = 'OWNRENT')
+plt.savefig('images1/credit2_boxplot.png')
+credit_card.boxplot('INCOME', by = 'OWNRENT')
+plt.savefig('images1/credit3_boxplot.png')
+credit_card.boxplot('INCOMESQ', by = 'OWNRENT')
+plt.savefig('images1/credit4_boxplot.png')
+credit_card.boxplot('AVGEXP')
+plt.savefig('images1/credit5_boxplot.png')
+
+
+#Models
+y1 = prestige['prestige']
+x1 = prestige[['income', 'education']].astype(float)
+x1 = sm.add_constant(x1)
+
+prest_model = sm.OLS(y1,x1).fit()
+summary1 = prest_model.summary()
+
+print summary1
+
+y2 = credit_card['AVGEXP']
+x2 = credit_card[['AGE', 'INCOME', 'INCOMESQ', 'OWNRENT']].astype(float)
+x2 = sm.add_constant(x2)
+
+credit_model = sm.OLS(y2,x2).fit()
+summary2 = credit_model.summary()
+
+print summary2
+
+
+'''
+
+2. Plot the studentized residuals against the fitted y-values.
+
+'''
+
+res1 = prest_model.outlier_test()['student_resid']
+res2 = credit_model.outlier_test()['student_resid']
+
+
+plt.scatter(prest_model.fittedvalues, res1)
+plt.xlabel('studentized residuals')
+plt.ylabel('predicted response')
+plt.axhline(y=0, c='r', ls='--')
+plt.savefig('images1/prest_residuals.png')
+
+plt.scatter(credit_model.fittedvalues, res2)
+plt.xlabel('studentized residuals')
+plt.ylabel('predicted response')
+plt.axhline(y=0, c='r', ls='--')
+plt.savefig('images1/credit_residuals.png')
+
+
+
+'''
+
+3. Take the log of `AVGEXP` in `credit card` data. Re-fit the model,re-plot the
+residuals.
+
+'''
+
+#Prestige Log
+y3 = np.log(y1)
+prest_model2 = sm.OLS(y3,x1).fit()
+res1_log = prest_model2.outlier_test()['student_resid']
+
+plt.scatter(prest_model2.fittedvalues, res1_log)
+plt.xlabel('studentized residuals with log response')
+plt.ylabel('predicted log response')
+plt.axhline(y=0, c='r', ls='--')
+plt.savefig('images1/logprest_residuals.png')
+
+
+#Credit Card Log
+y4 = np.log(y2)
+credit_model2 = sm.OLS(y4,x2).fit()
+res2_log = credit_model2.outlier_test()['student_resid']
+
+plt.scatter(credit_model2.fittedvalues, res2_log)
+plt.xlabel('studentized residuals with log response')
+plt.ylabel('predicted log response')
+plt.axhline(y=0, c='r', ls='--')
+plt.savefig('images1/logcredit_residuals.png')
+
+
+'''
+
+4. Make Q-Q plots for the studentized residuals of the `prestige` and `credit card`.
+
+'''
+
+sm.graphics.qqplot(res1, line='45', fit=True)
+plt.savefig('images1/pres1_qqplot.png')
+sm.graphics.qqplot(res2, line='45', fit=True)
+plt.savefig('images1/credit1_qqplot.png')
+sm.graphics.qqplot(res1_log, line='45', fit=True)
+plt.savefig('images1/pred2_qqplot.png')
+sm.graphics.qqplot(res2_log, line='45', fit=True)
+plt.savefig('images1/credit2_qqplot.png')
+
+'''
+
+5. Use the Variance Inflation Factor (VIF) to measure how collinear a
+particular feature is with the rest of the features. (VIF > 10)
+
+'''
+
+#Prestige
+prest_vif = []
+for index in range(x1.shape[1]):
+    prest_vif.append(round(variance_inflation_factor(x1.values, index),2))
+
+print prest_vif
+
+#Credit Card
+credit_vif = []
+for index in range(x2.shape[1]):
+    credit_vif.append(round(variance_inflation_factor(x2.values, index),2))
+
+print credit_vif
+
+
+## Part 2: Outlier Detection
+
+#Use of prestige dataset only the rest of assignment
+
+'''
+
+1. Plot (plotly) your residual plot for the prestige dataset. Identify and note
+the points that are more than 2 / -2 studentized residuals.
+
+'''
+
+trace = go.Scatter(
+    x = prest_model.fittedvalues,
+    y = res1,
+    mode = 'markers'
+)
+
+data = [trace]
+
+py.image.save_as({'data':data}, 'images1/inc_v_edu_iplot', format='png')
+
+'''
+
+2. Plot (plotly) the x-variables (income and education respectively) against
+the studentized residuals. Examine the outliers you have identified in `1`
+in the plots and explain why the points are identified as outliers
+
+'''
+trace1 = go.Scatter(
+    x = x1['income'],
+    y = res1,
+    mode = 'markers'
+)
+
+trace2 = go.Scatter(
+    x = x1['education'],
+    y = res1,
+    mode = 'markers'
+)
+
+data = [trace1, trace2]
+
+py.image.save_as({'data':data}, 'images1/prest_iplot', format='png')
+
+
+'''
+
+3. Make an influence plot and identify the outliers, the influential points,
+and the outliers with high influence.
+
+'''
+
+smg.influence_plot(prest_model)
+plt.savefig('images1/influence_plot.png')
+
+'''
+
+4. Remove the influential (high leverage) points identified in `3` and re-fit
+the model.
+
+'''
+prestige = prestige.drop(['conductor', 'RR.engineer', 'minister'])
+
+y = prestige['prestige']
+x = prestige[['income', 'education']].astype(float)
+x = sm.add_constant(x)
+
+prest_model2 = sm.OLS(y,x).fit()
+print prest_model2.summary()
---
+++ Linear_Regression/Exercise2.py
@@ -0,0 +1,221 @@
+import statsmodels.api as sm
+import pandas as pd
+import matplotlib.pyplot as plt
+
+## Linear Regression Problem
+
+'''
+
+1. Load the data into a dataframe from `data/balance.csv`.
+Make a scatter matrix of the variables.
+
+'''
+
+data = pd.read_csv('data/balance.csv', index_col=0)
+
+pd.scatter_matrix(data)
+plt.savefig('images2/balance_matrix.png')
+
+
+
+# The dataset contains no null values
+print data.isnull().sum()
+
+# The dataset contains 3 categorical columns (Gender, Student, Married, Ethnicity)
+# Ethinicity is the only column that contains more than 2 values.
+print data.info()
+print data['Gender'].unique()
+print data['Student'].unique()
+print data['Married'].unique()
+print data['Ethnicity'].unique()
+
+'''
+
+2. Convert Gender, Student and Married to 1/0. Create dummy variables for the
+Ethnicity column (pd.get_dummies()). After you create the model, drop the
+African American dummy variable.
+
+'''
+
+data['Gender'] = data['Gender'].map({' Male':1, 'Female':0})
+data['Married'] = data['Married'].map({'Yes':1, 'No':0})
+data['Student'] = data['Student'].map({'Yes':1, 'No':0})
+
+data = pd.get_dummies(data)
+del data['Ethnicity_African American']
+
+print data.info()
+
+'''
+
+3. Using all the feature variables, fit a linear regression model to predict
+`Balance`. Make a residual plot by plotting the fitted y values against the
+studentized residuals.
+
+'''
+
+x = data.copy()
+y = x.pop('Balance')
+
+all_model = sm.OLS(y,x).fit()
+print all_model.summary()
+
+resids = all_model.outlier_test()['student_resid']
+y_pred = all_model.predict(x)
+
+plt.scatter(y_pred, resids, label='Residual Plot')
+plt.savefig('images2/all_model_residuals.png')
+
+'''
+
+4. Try a few other models by excluding some features from the full model.
+Does the residual plot change?
+
+'''
+
+# Function that will fit and plot the model for any given x/y
+def plot_model(x, y, label):
+    model = sm.OLS(y,x).fit()
+    resids = model.outlier_test()['student_resid']
+    y_preds = model.predict(x)
+
+    plt.scatter(y_preds, resids, label=label)
+    plt.legend()
+    plt.show()
+
+# Loop through columns and remove each column and call plot function for each
+# removal.
+all_columns = x.columns
+for col_name in all_columns:
+    all_columns_copy = list(all_columns)
+    all_columns_copy.remove(col_name)
+    plot_model(x[all_columns_copy], y, 'Removed - ' + str(col_name))
+
+# Removing Income or Student had the most change in residuals
+
+# Looking at the Balance data, there are many values that are 0.
+y.hist(bins=100)
+plt.show()
+
+'''
+
+5. Re-plot the univariate scatter plot on a bigger figure size. Look for
+variable(s) that can differentiate most zero balance observation from non-zero
+balance observations.
+
+'''
+
+# Plots each column to find threshold limits to limit $0 for Balancec
+for col in all_columns:
+    data.plot(kind='scatter', y='Balance', x=col, edgecolor='none', figsize=(12, 5))
+    plt.xlabel(col)
+    plt.ylabel('Balance')
+    plt.show()
+
+# Limit > 2500, Rating > 200
+
+
+'''
+
+6. Remove the data points below the decided threshold of your chosen variable
+and examine the number of zero observations that remain. Now re-fit the same
+model and examine the residuals.
+
+'''
+
+cond1 = data['Limit'] >= 2500
+new_data = data[cond1]
+cond2 = new_data['Rating'] > 200
+new_data = new_data[cond2]
+
+print new_data.info()
+
+
+x2 = new_data.copy()
+y2 = x2.pop('Balance')
+
+new_model = sm.OLS(y2,x2).fit()
+print new_model.summary()
+
+resids2 = new_model.outlier_test()['student_resid']
+y_pred2 = new_model.predict(x2)
+
+plt.scatter(y_pred2, resids2, label='Residual Plot')
+plt.savefig('images2/new_model_residuals.png')
+
+
+## Extra Credit
+
+'''
+
+1. Fit a linear model to predict `Balance` using `Income` as a predictor.
+
+'''
+
+y = new_data['Balance']
+x = new_data['Income']
+
+income_model = sm.OLS(y,x).fit()
+print income_model.summary()
+
+
+'''
+
+2. Fit a second model using `Income` and `Student` as predictors.
+
+'''
+
+x = new_data[['Income', 'Student']]
+
+inc_stu_model = sm.OLS(y,x).fit()
+print inc_stu_model.summary()
+
+
+'''
+
+3. Finally fit a model using `Income`, `Student`, and `Income`*`Student`
+to account for a possible interaction
+
+'''
+
+new_data['Income*Student'] = new_data['Income'] * new_data['Student']
+x = new_data[['Income', 'Student', 'Income*Student']]
+
+inc_stu_model = sm.OLS(y,x).fit()
+print inc_stu_model.summary()
+
+'''
+
+4. Make a single plot with two regression lines, one for Student and one for non-Students.
+
+'''
+
+students = new_data['Student'] == 1
+nonstudents = new_data['Student'] == 0
+
+students_df = new_data.loc[students, ['Income', 'Balance']]
+nonstudents_df = new_data.loc[nonstudents, ['Income', 'Balance']]
+
+stu_x = students_df['Income']
+stu_y = students_df['Balance']
+
+nstu_x = nonstudents_df['Income']
+nstu_y = nonstudents_df['Balance']
+
+students_model = sm.OLS(stu_y,stu_x).fit()
+stu_pred = students_model.predict(stu_x)
+nonstudents_model = sm.OLS(nstu_y,nstu_x).fit()
+nstu_pred = nonstudents_model.predict(nstu_x)
+
+print students_model.summary()
+print nonstudents_model.summary()
+
+plt.scatter(stu_x, stu_y, color='r')
+plt.plot(stu_x, stu_pred, color='r')
+
+plt.scatter(nstu_x, nstu_y, color='b')
+plt.plot(nstu_x, nstu_pred, color='b')
+
+plt.xlabel('Income')
+plt.ylabel('Balance')
+plt.savefig('images2/student_v_nonstudent.png')
